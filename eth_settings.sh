80df3b8b263c00613e67ed318bb3794fa87604f3
Brendan ODowd <bodowd@worcester.edu>
2016-10-12 22:21:45 -0400

    Set txqueuelen on the interface

--- eth_settings.sh
+++ eth_settings.sh
@@ -5,7 +5,13 @@ eth_device=`ifconfig | sed -n '/addr:100.0/{g;H;p};H;x' | awk '{print $1}'`
 ethtool -K $eth_device lro on
 ethtool -A $eth_device tx off rx off
 ethtool -G $eth_device tx 8192 rx 8192
+ifconfig $eth_device txqueuelen 20000
+
+service irqbalance stop
 
 ethtool -k $eth_device | grep large-receive-offload
 ethtool -a $eth_device
 ethtool -g $eth_device
+ifconfig $eth_device | grep txqueuelen
+
+ps aux | grep irqbalance
