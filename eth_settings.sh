e10ec53ab7b8e6c3fe118eaef39155c9e9702a64
Brendan ODowd <bodowd@worcester.edu>
2016-10-12 23:15:04 -0400

    Add MTU size setting to eth script

--- eth_settings.sh
+++ eth_settings.sh
@@ -6,6 +6,7 @@ ethtool -K $eth_device lro on
 ethtool -A $eth_device tx off rx off
 ethtool -G $eth_device tx 8192 rx 8192
 ifconfig $eth_device txqueuelen 20000
+ifconfig $eth_device mtu 256
 
 service irqbalance stop
 
@@ -13,5 +14,6 @@ ethtool -k $eth_device | grep large-receive-offload
 ethtool -a $eth_device
 ethtool -g $eth_device
 ifconfig $eth_device | grep txqueuelen
+ifconfig $eth_device | grep MTU
 
 ps aux | grep irqbalance
