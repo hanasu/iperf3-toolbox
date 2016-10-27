9c37d4442d7536e8d4ed744d5bcaa88129774730
Brendan ODowd <bodowd@worcester.edu>
2016-10-27 00:52:40 -0400

    Add flag parsing to RDMA start_node

--- 1_to_n/rdma/start_node.sh
+++ 1_to_n/rdma/start_node.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#find out which blade number script is running on
 blade=`hostname | cut -c 6-7`
+#default transfer size of ib_write_bw if no argument passed
+size=8
+#how long the test will run for if no argument passed
+duration=20
 
-ib_write_bw -d mlx5_0 -i 1 -s 8 --report_gbits -D 20 -F -I 0 -t 1028 -Q 1 -p $((18513 + $blade)) 100.0.0.1 > `hostname`_to_blade1.log
+#parse command line arguments
+while getopts s:t: opt; do
+  case $opt in
+    s) size=$optarg
+      ;;
+    t) duration=$optarg
+      ;;
+  esac
+done
+
+ib_write_bw -d mlx5_0 -i 1 -s $size --report_gbits -D $duration -F -I 0 -t 1028 -Q 1 -p $((18513 + $blade)) 100.0.0.1 > `hostname`_to_blade1.log
