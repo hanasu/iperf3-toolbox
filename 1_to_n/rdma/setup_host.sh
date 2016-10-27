73ff88f9c9b7c84899ec88c11ca90029789bc3ff
Brendan ODowd <bodowd@worcester.edu>
2016-10-26 23:30:48 -0400

    Preliminary RDMA version

---
+++ 1_to_n/rdma/setup_host.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#default port is 18515 so let's start there
+port=18515
+#clean up any residual processes
+killall ib_write_bw
+
+#there are 15 remote nodes that will be connecting to this 1 server node
+#taskset assigns them to separate cpu cores for best performance
+#each loop will increment the process' port by 1
+for i in `seq 0 14`;
+do
+  taskset -c $i ib_write_bw -d mlx5_0 -p $port &
+  port=$(( $port + 1 ))
+done
---
+++ 1_to_n/rdma/start_node.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ib_write_bw -d mlx5_0 -i 1 -s 8 --report_gbits -D 20 -F -I 0 -t 1028 -Q 1 -R blade1 > `hostname`_to_blade1.log
