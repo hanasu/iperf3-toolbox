19014c52bbfdc6d9cde991ebfeb11a5f180674a0
Brendan ODowd <bodowd@worcester.edu>
2016-10-27 01:23:47 -0400

    Add log transfer from nodes to host

---
+++ 1_to_n/rdma/copy_client_logs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scp *.log root@blade1:/root/Desktop/iperf3-toolbox/1_to_n
--- 1_to_n/rdma/start_node.sh
+++ 1_to_n/rdma/start_node.sh
@@ -17,4 +17,9 @@ while getopts s:t: opt;do
   esac
 done
 
+#remove any residual processes from previous testing
+killall ib_write_bw
+
 ib_write_bw -d mlx5_0 -i 1 -s $size --report_gbits -D $duration -F -I 0 -t 1028 -Q 1 -p $((18513 + $blade)) 100.0.0.1 > `hostname`_to_blade1.log
+
+scp `hostname`_to_blade1.log root@blade1:/root/Desktop/iperf3-toolbox/1_to_n/rdma
