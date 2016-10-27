4d8f92e05223ea080183e8103feed7b716a570b9
Brendan ODowd <bodowd@worcester.edu>
2016-10-26 23:44:12 -0400

    Fix expression variable reference

--- 1_to_n/rdma/start_node.sh
+++ 1_to_n/rdma/start_node.sh
@@ -2,4 +2,4 @@
 
 blade=`hostname | cut -c 6-7`
 
-ib_write_bw -d mlx5_0 -i 1 -s 8 --report_gbits -D 20 -F -I 0 -t 1028 -Q 1 -p $((18513 + $hostname)) 100.0.0.1 > `hostname`_to_blade1.log
+ib_write_bw -d mlx5_0 -i 1 -s 8 --report_gbits -D 20 -F -I 0 -t 1028 -Q 1 -p $((18513 + $blade)) 100.0.0.1 > `hostname`_to_blade1.log
