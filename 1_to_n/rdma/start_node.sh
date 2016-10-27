edef123535da07f875fa50b5330190774f829640
Brendan ODowd <bodowd@worcester.edu>
2016-10-26 23:34:52 -0400

    Change hostname to IP to avoid rdma_cm ADDR_ERROR

--- 1_to_n/rdma/start_node.sh
+++ 1_to_n/rdma/start_node.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ib_write_bw -d mlx5_0 -i 1 -s 8 --report_gbits -D 20 -F -I 0 -t 1028 -Q 1 -R blade1 > `hostname`_to_blade1.log
+ib_write_bw -d mlx5_0 -i 1 -s 8 --report_gbits -D 20 -F -I 0 -t 1028 -Q 1 -R 100.0.0.1 > `hostname`_to_blade1.log
