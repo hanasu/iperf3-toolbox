34b4874776ae1b19c5c85432f4e7105ba92a3ea6
Brendan ODowd <bodowd@worcester.edu>
2016-10-26 23:41:42 -0400

    Add port increment handling on node side

--- 1_to_n/rdma/start_node.sh
+++ 1_to_n/rdma/start_node.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ib_write_bw -d mlx5_0 -i 1 -s 8 --report_gbits -D 20 -F -I 0 -t 1028 -Q 1 100.0.0.1 > `hostname`_to_blade1.log
+blade=`hostname | cut -c 6-7`
+
+ib_write_bw -d mlx5_0 -i 1 -s 8 --report_gbits -D 20 -F -I 0 -t 1028 -Q 1 -p $((18513 + $hostname)) 100.0.0.1 > `hostname`_to_blade1.log
