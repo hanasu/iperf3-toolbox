7964d5d7f32016e92f0c9e5d381d5c2d4319f2e3
Brendan ODowd <bodowd@worcester.edu>
2016-10-31 23:57:31 -0400

    Cleaned up and formatted output of eth_settings script

--- eth_settings.sh
+++ eth_settings.sh
@@ -2,7 +2,10 @@
 
 #find out what device is assigned to the 100.0.0.X address thus is 100G
 eth_device=`ifconfig | sed -n '/addr:100.0/{g;H;p};H;x' | awk '{print $1}'`
+#default MTU if no argument passed on command line
 mtu=1500
+#number of cores available on system
+cores=`cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_cur_freq | wc -l`
 
 #handle parsing the -m argument for MTU setting
 while getopts m: opt; do
@@ -11,6 +14,10 @@ while getopts m: opt; do
       ;;
   esac
 done
+
+echo "OUTPUT"
+echo "-----------------------"
+echo ""
 
 #set large-receive-offload on, turn off pause frames, increase ring NIC buffers
 ethtool -K $eth_device lro on
@@ -30,10 +37,26 @@ service irqbalance stop
 #disable the firewall to prevent connection issues
 /sbin/rcSuSEfirewall2 stop
 
+#for each core on the system (zero-indexed) set the governor to performance
+#to prevent CPU throttling for energy savings
+for i in `seq 0 $(( cores - 1 ))`; do
+  echo performance > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor;
+done
+
+echo ""
+echo "STATUS"
+echo "-----------------------"
+
 #check the current status of all the settings just changed to verify
-ethtool -k $eth_device | grep large-receive-offload
+echo "Large Receive Offload:`ethtool -k $eth_device | grep large-receive-offload | cut -d ':' -f 2`"
+echo ""
 ethtool -a $eth_device
 ethtool -g $eth_device
-ifconfig $eth_device | grep txqueuelen
-ifconfig $eth_device | grep MTU
-ps aux | grep irqbalance
+echo "TX queue length: `ifconfig $eth_device | grep txqueuelen | cut -d ':' -f 3`"
+echo "MTU: `ifconfig $eth_device | grep MTU | cut -d ':' -f 2 | cut -c 1-4`"
+if pgrep "irqbalance" > /dev/null
+then
+  echo "IRQbalance: Running"
+else
+  echo "IRQbalance: Stopped"
+fi
