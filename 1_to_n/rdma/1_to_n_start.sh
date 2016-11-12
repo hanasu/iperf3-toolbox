9eef557a7538ff44d0197a39bb50487d05562931
Brendan ODowd <bodowd@worcester.edu>
2016-11-11 19:49:59 -0500

    Set default QP value and add QP flag on command line

--- 1_to_n/rdma/1_to_n_start.sh
+++ 1_to_n/rdma/1_to_n_start.sh
@@ -4,17 +4,22 @@
 size=8
 #how long the test will run in seconds if no argument passed
 duration=20
+#number of queue pairs to use if no argument passed
+qps=25
 
 #parse command line arguments
-while getopts s:t: opt;do
+while getopts s:t:q: opt;do
   case $opt in
     s) size=$OPTARG
       ;;
     t) duration=$OPTARG
       ;;
+    q)
+       qps=$OPTARG
+      ;;
   esac
 done
 
 for i in `seq 2 16`; do
-  ib_write_bw -d mlx5_0 -i 1 -s $size --report_gbits -D $duration -F -I 0 -t 1028 -Q 25 -p $((18528 + $i)) -b 100.0.0.$i > blade1_to_blade${i}.log &
+  ib_write_bw -d mlx5_0 -i 1 -s $size --report_gbits -D $duration -F -I 0 -t 1028 -Q $qps -p $((18528 + $i)) -b 100.0.0.$i > blade1_to_blade${i}.log &
 done
--- 1_to_n/rdma/n_to_1_start.sh
+++ 1_to_n/rdma/n_to_1_start.sh
@@ -6,15 +6,19 @@ blade=`hostname | cut -c 6-7`
 size=8
 #how long the test will run in seconds if no argument passed
 duration=20
+#number of queue pairs to use if no argument passed
+qps=25
 
 #parse command line arguments
-while getopts s:t: opt;do
+while getopts s:t:q: opt;do
   case $opt in
     s) size=$OPTARG
       ;;
     t) duration=$OPTARG
       ;;
+    q) qps=$OPTARG
+      ;;
   esac
 done
 
-ib_write_bw -d mlx5_0 -i 1 -s $size --report_gbits -D $duration -F -I 0 -t 1028 -Q 25 -p $((18513 + $blade)) -b 100.0.0.1
+ib_write_bw -d mlx5_0 -i 1 -s $size --report_gbits -D $duration -F -I 0 -t 1028 -Q $qps -p $((18513 + $blade)) -b 100.0.0.1
